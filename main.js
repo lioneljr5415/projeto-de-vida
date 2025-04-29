18733cb0202615a01a5a21e9989a68c434eaa4a3
lioneljr5415 <136764104+lioneljr5415@users.noreply.github.com>
2025-04-29 09:05:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".abas-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
+for(let i=0;i <botoes.length;i++){
+     botoes[i].onclick = function() {
 
-    for(let j=0;j<botoes.length;j++) {
+    for(let j=0;j<botoes.length; j++) {
     botoes[j].classList.remove("ativo");
     textos[j].classList.remove("ativo")
 
@@ -14,3 +15,9 @@ for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
     
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
