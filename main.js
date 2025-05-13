4c5cee0b120707a54533a1f7e33d3a4e0194753a
lioneljr5415 <136764104+lioneljr5415@users.noreply.github.com>
2025-05-13 08:58:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -17,10 +17,25 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2022-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-10-05T00:00:00");
 
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+ } 
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000); }
+    comecaCronometro();
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
